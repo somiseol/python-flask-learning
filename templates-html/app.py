b59cbba56daa777cad97fa5cd51a5b698317d55e
somiseol <somi.seol@proton.me>
2026-03-04 20:24:54 -0600

    learned about inherting templates

--- templates-html/app.py
+++ templates-html/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+# render_template(template_name_or_list) is for returning html file instead of plain text in functions like index()
 
 app = Flask(__name__, template_folder='templates')
 # templates directory
 
 @app.route('/')
 def index():
-    return 'hello world'
+    myvalue = 'lorem'
+    myresult = 10 + 20
+    mylist = [1, 2, 3, 4, 5]
+    return render_template('index.html', myvalue=myvalue, myresult=myresult, random_value='ipsum', mylist=mylist) # values will be passed on to the html template
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
